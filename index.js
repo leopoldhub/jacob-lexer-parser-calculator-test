1f563a608f4f099f177f4548ecdf09379f699631
leopoldhub <hubertleopold01@gmail.com>
2022-07-19 04:07:29 +0200

    add pow, mod, min and max

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const CalculatorParser = require("./grammar.jacobgram.out");
 const parser = new CalculatorParser();
 
 lexer.setInput(
-	"(10 / 2) + 3 /* this is a comment\n   on multiple lines */ - 5"
+	"((10 / 2) + 3 /* this is a comment\n   on multiple lines */ - 5) MAX 9 % 5 ^ 2"
 );
 parser.parse(lexer);
